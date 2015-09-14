e627b41040e0d6fe0cf3e308920cd5621d1c0a2d
Asad <msasad.visitor@iitd.ac.in>
2015-09-14 13:03:19 +0530

    Implement approve request

--- controllers/admin.py
+++ controllers/admin.py
@@ -12,7 +12,14 @@ def pending_requests_list():
 def handle_request():
     status = {'edit':'edited','approve': 'approved', 'reject' : 'rejected'}
     rid = request.vars.id
-    action = request.vars.request
+    action = request.vars.action
     if action == 'approve':
-        
+        try:
+            row = db(db.vm_requests.id == request.vars.id).select()[0]
+            vm = conn.createBaadalVM(row.name, row.image, row.flavor)
+            if vm:
+                return json.dumps({'status':'OK'})
+        except Exception as e:
+            return json.dumps({'status': 'fail','message':e.message})
+        #vm = conn.createBaadalVM()
     return json.dumps({'message':'Request id %s is %s'%(request.vars.id,status[request.vars.action])})
