c06205bc134c381d3aff62d16b733f8c3ea2744e
Mohammad Asad Siddiqui <msasad@users.noreply.github.com>
2015-11-13 14:22:12 +0530

    Delete client.py

--- modules/client.py
+++
@@ -1,7 +0,0 @@
-import Baadal
-from pprint import pprint
-_authurl = 'http://192.168.56.102:35357/v2.0'
-_tenant = 'demo'
-_username = 'tester'
-_password = 'baadal'
-conn = Baadal.Connection(_authurl, _tenant, _username, _password)
