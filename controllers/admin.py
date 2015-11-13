7d8c37b201df5ddafb5d3d4d6f86906a5bc68000
Asad <msasad8@gmail.com>
2015-11-13 11:38:40 +0530

    Use flavor info_function to fetch info string for a flavor

--- controllers/admin.py
+++ controllers/admin.py
@@ -1,13 +1,11 @@
 import json
-from log_handler import logger
-
-conn = Baadal.Connection(1, 2, 3, 4)
 
 
 def pending_requests():
     rows = db(db.vm_requests.state == 0).select()
     l = rows.as_list()
     for i in l:
+        i['flavor'] = flavor_info(i['flavor'])
         i['sec_domain'] = network_name_from_id(i['sec_domain'])
         i['request_time'] = seconds_to_localtime(i['request_time'])
         i['public_ip_required'] = 'Required' if i['public_ip_required'] == 1 else 'Not Required'
