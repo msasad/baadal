d54705abafd268609b7a25dd147e0b213dcb97b8
Asad <msasad8@gmail.com>
2015-11-17 14:57:12 +0530

    Set username in session

--- models/connection.py
+++ models/connection.py
@@ -2,13 +2,14 @@ import Baadal
 import ConfigParser
 config = ConfigParser.ConfigParser()
 config.read('/etc/baadal/baadal.conf')
-_authurl = config.get('auth','authurl')
-_tenant = config.get('auth','tenant')
-_password = config.get('auth','password')
+_authurl = config.get('auth', 'authurl')
+_tenant = config.get('auth', 'tenant')
+_password = config.get('auth', 'password')
 _username = config.get('auth', 'username')
 conn = Baadal.Connection(_authurl, _tenant, _username, _password)
 
-
+# FIXME Remove this line
+session.username = _username
 
 EXTERNAL_NETWORK = config.get('misc', 'external_network_name')
 
