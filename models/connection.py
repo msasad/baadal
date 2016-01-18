c176fbb61d32940da5f604a6cc0ff5f7fef8c5ba
Asad <msasad8@gmail.com>
2016-01-18 14:38:10 +0530

    Remove hardcoded authentication

--- models/connection.py
+++ models/connection.py
@@ -4,12 +4,7 @@ config = ConfigParser.ConfigParser()
 config.read('/etc/baadal/baadal.conf')
 _authurl = config.get('auth', 'authurl')
 _tenant = config.get('auth', 'tenant')
-_password = config.get('auth', 'password')
-_username = config.get('auth', 'username')
-conn = Baadal.Connection(_authurl, _tenant, _username, _password)
 
 # FIXME Remove this line
-session.username = _username
-
 EXTERNAL_NETWORK = config.get('misc', 'external_network_name')
 
