d207958187fa6fbf59fe66c4937f90c99dd9fb1f
Asad <msasad8@gmail.com>
2016-01-18 14:35:30 +0530

    Create function to handle custom login

--- controllers/default.py
+++ controllers/default.py
@@ -1,3 +1,4 @@
+@auth.requires_login()
 def index():
     form = FORM(INPUT(_name='vmid', _id='vmid'),
                 INPUT(_class='task-button', _type='button', _id='start', _name='start', _value='start'),
@@ -32,8 +33,34 @@ def index():
     return dict(form=form)
 
 
+@auth.requires_login()
 def home():
     return dict()
 
 def user():
-    return dict(form=auth())
+    if request.vars._formname == 'login':
+        session.username = request.vars.username
+        session.password = request.vars.password
+    if request.args(0) == 'not_authorized':
+        raise HTTP(403, 'forbidden')
+    else:
+        if auth.user and request.args(0) in ('login', None):
+            redirect('/baadal/user')
+        from gluon.html import INPUT, H2
+        form = auth()
+        el = list()
+        el.append(H2('Please log in')) 
+        label = LABEL('Email address')
+        label.attributes['_for'] = 'auth_user_username'
+        label.add_class('sr-only')
+        el.append(label)
+        for i in form.elements():
+            if isinstance(i, INPUT):
+                if i.attributes['_type'] in ('text','password'):
+                    i.add_class('form-control')
+                if i.attributes['_type'] == 'submit':
+                    i.add_class('btn btn-primary btn-lg btn-block')
+                el.append(i)
+        form.components = el
+        form.add_class('form-signin')
+        return dict(form=form)
