93a80b03cd1c83bb5bd6e2eb6fb90629c2ecd43a
Asad <msasad8@gmail.com>
2016-02-04 16:55:42 +0530

    Add controller function for user registration form

--- controllers/user.py
+++ controllers/user.py
@@ -11,17 +11,20 @@ def request():
 def login():
     return dict()
 
+
 @auth.requires_login()
 def my_vms():
     try:
-        conn = Baadal.Connection(_authurl, _tenant, session.username, session.password)
+        conn = Baadal.Connection(_authurl, _tenant, session.username,
+                                 session.password)
         vms = conn.baadal_vms()
         response = list()
         for vm in vms:
             vm_properties = vm.properties()
             snapshots = vm.properties()['snapshots']
             for index in range(0, len(snapshots)):
-                snapshots[index]['created'] = convert_timezone(snapshots[index]['created'])
+                snapshots[index]['created'] = convert_timezone(
+                    snapshots[index]['created'])
             vm_properties['snapshots'] = snapshots
             response.append(vm_properties)
         return jsonify(data=response)
@@ -38,7 +41,8 @@ def my_vms():
 @auth.requires_login()
 def vm_status():
     try:
-        conn = Baadal.Connection(_authurl, _tenant, session.username, session.password)
+        conn = Baadal.Connection(_authurl, _tenant, session.username,
+                                 session.password)
         vmid = request.vars.vmid
         vm = conn.find_baadal_vm(id=vmid)
         if vm:
@@ -60,10 +64,15 @@ def my_requests():
         i['flavor'] = flavor_info(i['flavor'])
         i['sec_domain'] = network_name_from_id(i['sec_domain'])
         i['request_time'] = seconds_to_localtime(i['request_time'])
-        i['public_ip_required'] = 'Required' if i['public_ip_required'] == 1 else 'Not Required'
+        i['public_ip_required'] = 'Required' if i['public_ip_required'] == 1 \
+            else 'Not Required'
     return jsonify(data=l)
 
 
 @auth.requires_login()
 def my_requests_list():
+    return dict()
+
+
+def register():
     return dict()
