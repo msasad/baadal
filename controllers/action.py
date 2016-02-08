6eba47545736453786e14edeb8200416f91df060
Asad <msasad8@gmail.com>
2016-02-08 16:18:12 +0530

    Implement user approval

--- controllers/action.py
+++ controllers/action.py
@@ -144,7 +144,7 @@ def index():
         return  __migrate(vmid)
 
 
-@auth.requires_login()
+@auth.requires(user_is_project_admin)
 def handle_request():
     action = request.vars.action
     if action == 'approve':
@@ -255,3 +255,26 @@ def __faculty_approve():
     return jsonify()
 
 
+@auth.requires(user_is_project_admin)
+def handle_account_request():
+    try:
+        l = BaadalLDAP.BaadalLDAP(ldap_host, ldap_base_dn, ldap_admin_dn, ldap_admin_password)
+        row = db(db.account_requests.id == request.vars.id).select()[0]
+        username = row.username
+        user_is_faculty = bool(row.faculty_privileges)
+        password = row.password
+        email = row.email
+        userid = row.userid
+        l.add_user(username, userid, password, user_is_faculty=user_is_faculty, email=email)
+        conn = Baadal.Connection(_authurl, _tenant, session.username, session.password)
+        conn.add_user_role(userid, _tenant, 'user')
+        row.update_record(approval_status = 1)
+        return jsonify()
+    except Exception as e:
+        logger.error(e.message or str(e.__class__))
+        return jsonify(status='fail', message=e.message or str(e.__class__))
+    finally:
+        try:
+            conn.close()
+        except:
+            pass
