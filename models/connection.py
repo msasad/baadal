1c4fea49a20b75efd915d75d86904c14ad7d5bf0
Asad <msasad8@gmail.com>
2016-02-08 16:20:36 +0530

    Add LDAP connection parameters

--- models/connection.py
+++ models/connection.py
@@ -1,9 +1,16 @@
 import Baadal
 import ConfigParser
+import BaadalLDAP
 config = ConfigParser.ConfigParser()
 config.read('/etc/baadal/baadal.conf')
 _authurl = config.get('auth', 'authurl')
 _tenant = config.get('auth', 'tenant')
+
+ldap_host = config.get('ldap', 'ldap_host')
+ldap_dn = config.get('ldap', 'ldap_dn')
+ldap_base_dn = config.get('ldap', 'ldap_base_dn')
+ldap_admin_dn = config.get('ldap', 'ldap_admin_dn')
+ldap_admin_password = config.get('ldap', 'ldap_admin_password')
 
 # FIXME Remove this line
 EXTERNAL_NETWORK = config.get('misc', 'external_network_name')
