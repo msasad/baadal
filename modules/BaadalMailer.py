6bba9c0452e8226212ccbcda63ae73c55949df0a
Asad <msasad8@gmail.com>
2016-02-29 10:55:09 +0530

    Add mail subjects and body templates

--- modules/BaadalMailer.py
+++ modules/BaadalMailer.py
@@ -1,4 +1,6 @@
 from gluon.tools import Mail
+import gluon
+
 
 class BaadalMailer(object):
     class MailTypes(object):
@@ -13,16 +15,28 @@ class BaadalMailer(object):
             mailbody = ''
 
         class VMRequest(object):
-            subject = ''
-            mailbody = ''
+            subject = 'VM request successful'
+            mailbody = "Dear BaadalUser, you vm request has been posted \
+                successfully and is waiting for approval. You will be \
+                notified with a separate email after the VM is created."
 
         class ApprovalReminder(object):
             subject = ''
             mailbody = ''
 
         class VMCreated(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Your BaadalVM has been created'
+            mailbody =  "Dear {0[userName]},\n\n The VM {0[vmName]} requested \
+                    on {0[requestTime]} is successfully created and is now \
+                    available for use. Following operations are allowed on \
+                    VM:\n 1. Start\n2. Stop\n3. Pause\n4. Resume\n5. Destroy \
+                    \n6. Delete\n\n Default credentials for VM is as follows: \
+                    \nUsername:root/baadalservervm/baadaldesktopvm\n \
+                    Password:baadal\n\n To access VM using assigned private
+                    IP; SSH to baadal gateway machine using your GCL credential
+                    .\n username@{0[gatewayVM]}\n For other details, Please 
+                    login to baadal WEB interface."
+
 
         class TaskComplete(object):
             subject = ''
@@ -45,8 +59,9 @@ class BaadalMailer(object):
             mailbody = ''
 
         class RegistrationSuccessful(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Baadal Registration Successful'
+            mailbody = 'Dear BaadalUser, Your Baadal account has been \
+                    activated. Welcome to Baadal!'
 
     class SUBJECT(object):
         REGISTRATION_SUCCESSFUL = 'Baadal Registration Successful'
@@ -56,7 +71,7 @@ class BaadalMailer(object):
         VM_CREATION = "VM created successfully"
         TASK_COMPLETE = "{0[taskType]} task successful"
         VNC_ACCESS = "VNC Access to your VM activated"
-        DELETE_WARNING = "Delete Warning to the Shutdown VM" 
+        DELETE_WARNING = "Delete Warning to the Shutdown VM"
         SHUTDOWN_WARNING = "Shutdown Warning to the unused VM"
         BAADAL_SHUTDOWN = "VM Shutdown notice"
 
@@ -75,23 +90,14 @@ class BaadalMailer(object):
             has made a '{0[requestType]}' request on {0[requestTime]}. It is
             waiting for your approval."""
 
-        
-    def __init__(self,):
-        global mail_server, mail_sender, mail_username, mail_password
-        self.mailer = gluon.tools.Mail()
+    def __init__(self, mail_server, mail_sender, mail_login=None):
+        self.mailer = Mail()
+        self.mailer.settings.server = mail_server
         self.mailer.settings.sender = mail_sender
-        self.mailer.settings.login = '%s:%s' % (mail_username, mail_password)
-        pass
-    
-    def send(mail_type, recipient, cc=[],):
-        assert mail_type in [ i for i in dir(BaadalMailer.MailTypes) if not \
-                i.startswith('__') ]
+        self.mailer.settings.login = mail_login
+
+    def send(self, mail_type, recipient, cc=[]):
         subject = mail_type.subject
         mailbody = mail_type.mailbody
-
-        self.mailer.send(recipient.username
-
-        subject = getattr(BaadalMailer.SUBJECT, mail_type)
-        mailbody = getattr(BaadalMailer.MAILBODY, mail_type)
-
-    def  
+        return self.mailer.send(to=recipient, subject=subject,
+                                message=mailbody)
