cce06b2b012150fca0389c8ebbac8dbcea8caac4
Asad <msasad8@gmail.com>
2016-03-01 16:26:45 +0530

    Add global config for mail_support address and gateway_server

--- models/db.py
+++ models/db.py
@@ -19,6 +19,9 @@ ldap = BaadalLDAP(ldap_host, ldap_base_dn, ldap_admin_dn,
 mail_server = config.get('mail', 'server')
 mail_sender = config.get('mail', 'sender')
 mail_login = config.get('mail', 'login')
+mail_support = config.get('mail', 'support')
+
+gateway_server = config.get('misc', 'gateway_server')
 
 mailer = BaadalMailer(mail_server, mail_sender, mail_login)
 
