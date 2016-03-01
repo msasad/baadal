f97cbcdef67751d7014854d7854e5a105ae4498d
Asad <msasad8@gmail.com>
2016-03-01 11:40:11 +0530

    Update email subjects and message bodies

--- modules/BaadalMailer.py
+++ modules/BaadalMailer.py
@@ -1,5 +1,4 @@
 from gluon.tools import Mail
-import gluon
 
 
 class BaadalMailer(object):
@@ -11,8 +10,10 @@ class BaadalMailer(object):
                 Baadal!"""
 
         class RegistrationDenied(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Baadal Registration Denied'
+            mailbody = "Dear {0[userName]},\n\nYour registration to Baadal \
+                    has been denied. For any query, send an email to \
+                    {0[supportMail]}" 
 
         class VMRequest(object):
             subject = 'VM request successful'
@@ -21,47 +22,63 @@ class BaadalMailer(object):
                 notified with a separate email after the VM is created."
 
         class ApprovalReminder(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Request waiting for your approval'
+            mailbody = "Dear {0[approverName]},\n\n{0[userName]} has made a \
+                    '{0[requestType]}' request on {0[requestTime]}. It is \
+                    waiting for your approval."
 
         class VMCreated(object):
             subject = 'Your BaadalVM has been created'
-            mailbody =  "Dear {0[userName]},\n\n The VM {0[vmName]} requested \
+            mailbody = "Dear {0[userName]},\n\n The VM {0[vmName]} requested \
                     on {0[requestTime]} is successfully created and is now \
                     available for use. Following operations are allowed on \
                     VM:\n 1. Start\n2. Stop\n3. Pause\n4. Resume\n5. Destroy \
                     \n6. Delete\n\n Default credentials for VM is as follows: \
                     \nUsername:root/baadalservervm/baadaldesktopvm\n \
-                    Password:baadal\n\n To access VM using assigned private
-                    IP; SSH to baadal gateway machine using your GCL credential
-                    .\n username@{0[gatewayVM]}\n For other details, Please 
-                    login to baadal WEB interface."
+                    Password:baadal\n\n To access VM using assigned private \
+                    IP; SSH to baadal gateway machine using your GCL \
+                    credential .\n username@{0[gatewayVM]}\n For other\
+                    details, Please login to baadal WEB interface."
 
 
         class TaskComplete(object):
-            subject = ''
-            mailbody = ''
+            subject = '{0[taskType]} task successful'
+            mailbody = "Dear {0[userName]},\n\n The '{0[taskType]}' task for\
+                    VM({0[vmName]}) requested on {0[requestTime]} is complete."
 
         class VNCAccessActivated(object):
-            subject = ''
-            mailbody = ''
+            subject = 'VNC Access to your VM activated'
+            mailbody = 'Dear {0[userName]},\n\n VNC Access to your VM \
+                    {0[vmName]} was activated on {0[requestTime]}. Details \
+                    follow:\n 1. VNC IP : {0[vncIP]}\n2. VNC Port  :  \
+                    {0[vncPort]}\n\nVNC Access will be active for 30 minutes \
+                    only.\n\n For other details, Please login to baadal WEB \
+                    interface.'
 
         class VMDeleteWarning(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Delete Warning to the Shutdown VM'
+            mailbody = "Dear {0[userName]},\n\n It has been noticed that your\
+                    VM {0[vmName]} is being shutdown from {0[vmShutdownDate]}.\
+                    Kindly use the VM/delete the VM if not required. \n\ If no\
+                    action is taken on the VM, the VM will be automatically\
+                    deleted on {0[vmActionDate]}. \n\n For other details,\
+                    Please login to baadal WEB interface."
 
         class ShutdownBaadal(object):
-            subject = ''
-            mailbody = ''
+            subject = 'Baadal Shutdown notice'
+            mailbody = "Dear {0[userName]},\n\n Baadal services will be\
+                    shutting down today from 12:00 PM to 6:00 PM for\
+                    maintenance. We will shutdown your VM\
+                    {0[vmName]}({0[vmIp]}) to avoid any corruption of data.\
+                    VM will be brought up as soon as possible."
 
         class VMShutdownWarning(object):
             subject = ''
             mailbody = ''
 
-        class RegistrationSuccessful(object):
-            subject = 'Baadal Registration Successful'
-            mailbody = 'Dear BaadalUser, Your Baadal account has been \
-                    activated. Welcome to Baadal!'
+        mail_footer = "\n\nRegards,\nBaadal Admin\n\n\nNOTE: Please do not\
+                reply to this email. It corresponds to an unmonitored mailbox.\
+                If you have any queries, send an email to {0[adminEmail]}."
 
     class SUBJECT(object):
         REGISTRATION_SUCCESSFUL = 'Baadal Registration Successful'
