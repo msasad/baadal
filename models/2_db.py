8c908903c32a2c731ff77b4a4fabdef1f7f5690d
Asad <msasad8@gmail.com>
2016-04-01 11:04:25 +0530

    Change request_time field datatype to datetime

--- models/2_db.py
+++ models/2_db.py
@@ -77,8 +77,7 @@ db.define_table('virtual_disk_requests',
                 Field('id', 'integer'),
                 Field('user', 'string', required=True),
                 Field('vmid', 'string', required=True),
-                Field('request_time', 'integer',
-                      requires=IS_INT_IN_RANGE(0, 1)),
+                Field('request_time', 'datetime'),
                 Field('status', 'integer'),
                 Field('disk_size', 'integer')
                 )
