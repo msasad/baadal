5c565ae8ef9e39ddeae4d07c09218ae1711e5900
Asad <msasad8@gmail.com>
2016-05-04 11:23:38 +0530

    Rename pending request page

--- controllers/admin.py
+++ controllers/admin.py
@@ -4,16 +4,18 @@ import gluon
 
 @auth.requires(user_is_project_admin)
 def pending_requests():
-    rows = db(db.vm_requests.state < 2).select()
-    l = rows.as_list()
-    pub_ip = 'public_ip_required'
-    for i in l:
-        i['flavor'] = flavor_info(i['flavor'])
-        i['sec_domain'] = network_name_from_id(i['sec_domain'])
-        i['request_time'] = seconds_to_localtime(i['request_time'])
-        i[pub_ip] = 'Required' if i[pub_ip] == 1 else 'Not Required'
-    return json.dumps({'data': l})
-    pass
+    if request.extension in ('', 'html', None):
+        return dict()
+    elif request.extension == 'json':
+        rows = db(db.vm_requests.state < 2).select()
+        l = rows.as_list()
+        pub_ip = 'public_ip_required'
+        for i in l:
+            i['flavor'] = flavor_info(i['flavor'])
+            i['sec_domain'] = network_name_from_id(i['sec_domain'])
+            i['request_time'] = seconds_to_localtime(i['request_time'])
+            i[pub_ip] = 'Required' if i[pub_ip] == 1 else 'Not Required'
+        return json.dumps({'data': l})
 
 
 @auth.requires(user_is_project_admin)
