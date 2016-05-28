5aab8dbaaf5fdeb037a46631f065c4867c8a6362
Asad <msasad8@gmail.com>
2016-05-28 22:31:03 +0530

    Code clean up

--- static/js/baadal.js
+++ static/js/baadal.js
@@ -1,123 +1,116 @@
-var baadalApp = Object();
-baadalApp.serialize = function (temp) {
-  var obj = {};
-  for (var i in temp) {
-    var curr = temp[i];
-    obj[curr.name] = curr.value;
-  }
-  return obj;
-};
+/*global
 
-baadalApp.generateTicketLink = function(error) {
-  console.log('http://127.0.0.1:8000/admin/default/ticket/' + error.split(' ')[1]);
-};
+console,
+Handlebars
+*/
 
-baadalApp.ipArrayToString = function(data, usebr) {
-  if (typeof data == 'string') return data;
-  var arr = [];
-  for (var i = data.length - 1; i>=0; i--) {
-    var str = '';
-    var keys = Object.keys(data[i]);
-    for (var j = keys.length - 1; j >= 0; j--) {
-      str += keys[j] + ':' + data[i][keys[j]] + ' ';
+var baadalApp = (function () {
+  'use strict';
+  baadalApp = {};
+  baadalApp.serialize = function (temp) {
+    var obj = {}, i, curr;
+    for (i = temp.length - 1; i >= 0; i -= 1) {
+      curr = temp[i];
+      obj[curr.name] = curr.value;
     }
-    arr.push(str);
-  }
-  return arr.join(usebr ? '<br>' : '\n');
-};
+    return obj;
+  };
 
-baadalApp.ipObjectToString = function(object) {
-  strings = [];
-  for (var i in object) {
-    ips = object[i];
-    for (var ip of ips) {
-      strings.push('MAC: ' + ip['OS-EXT-IPS-MAC:mac_addr'] + ' IP: ' + ip['addr']);
+  baadalApp.generateTicketLink = function (error) {
+    console.log(document.location.origin + '/admin/default/ticket/' + error.split(' ')[1]);
+  };
+
+  baadalApp.ipArrayToString = function (ipArray, usebr) {
+    if (!Array.isArray(ipArray)) {
+      return ipArray;
     }
-  }
-  return strings.join('<br/>')
-};
+    var arr = [], i, str, keys, j;
+    ipArray.forEach(function (ip, index, full) {
+      str = '';
+      keys = Object.keys(ip);
+      keys.forEach(function (el) {
+        str += el + ':' + full[index][el] + ' ';
+      });
+      arr.push(str);
+    });
+    return arr.join(usebr ? '<br>' : '\n');
+  };
+
+  baadalApp.ipObjectToString = function (object) {
+    var strings = [], i, ips, ip;
+    Object.keys(object).forEach(function (network) {
+      object[network].forEach(function (ip) {
+        strings.push('MAC: ' + ip['OS-EXT-IPS-MAC:mac_addr'] + ' IP: ' + ip.addr);
+      });
+    });
+    return strings.join('<br/>');
+  };
+
+
+  baadalApp.filterArrayObject = function (object, array_name, field_name, value) {
+    if (object.hasOwnProperty(array_name)) {
+      var array = object[array_name], newarray = [];
+      array.forEach(function (elem) {
+        if (elem[field_name] === value) {
+          newarray.push(elem);
+        }
+      });
+      object[array_name] = newarray;
+      return object;
+    }
+  };
+
+  baadalApp.populateSnapshots = function (data, template) {
+    template = Handlebars.compile(template);
+    return template(data);
+  };
+
+  baadalApp.getSettingsButtonsMarkup = function (status, template) {
+    var actions = {
+      'ACTIVE': 'shutdown',
+      'SHUTOFF': 'start',
+      'ERROR': 'start'
+    },
+      glyphicons = {
+        'ACTIVE': 'stop',
+        'SHUTOFF': 'play',
+        'ERROR': 'play'
+      };
+    return '<div class="btn-group" role="group" aria-label="..."><button title="' +
+      actions[status] +
+      '" type="button" data-action="' +
+      actions[status] +
+      '" class="btn btn-success btn-action" aria-label="Start"><span class="fa fa-' +
+      glyphicons[status] +
+      '" aria-hidden="true"></span></button><button title="Settings" type="button"' +
+      'data-action="settings" class="btn btn-primary btn-settings" aria-label="Settings">' +
+      '<span class="fa fa-cog" aria-hidden="true"></span></button></div>';
+  };
+  return baadalApp;
+}());
 
 if (Handlebars) {
   Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+    'use strict';
     switch (operator) {
-      case '==':
-        return (v1 == v2) ? options.fn(this) : options.inverse(this);
-      case '===':
-        return (v1 === v2) ? options.fn(this) : options.inverse(this);
-      case '<':
-        return (v1 < v2) ? options.fn(this) : options.inverse(this);
-      case '<=':
-        return (v1 <= v2) ? options.fn(this) : options.inverse(this);
-      case '>':
-        return (v1 > v2) ? options.fn(this) : options.inverse(this);
-      case '>=':
-        return (v1 >= v2) ? options.fn(this) : options.inverse(this);
-      case '&&':
-        return (v1 && v2) ? options.fn(this) : options.inverse(this);
-      case '||':
-        return (v1 || v2) ? options.fn(this) : options.inverse(this);
-      default:
-        return options.inverse(this);
+    case '==':
+      return (v1 == v2) ? options.fn(this) : options.inverse(this);
+    case '===':
+      return (v1 === v2) ? options.fn(this) : options.inverse(this);
+    case '<':
+      return (v1 < v2) ? options.fn(this) : options.inverse(this);
+    case '<=':
+      return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+    case '>':
+      return (v1 > v2) ? options.fn(this) : options.inverse(this);
+    case '>=':
+      return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+    case '&&':
+      return (v1 && v2) ? options.fn(this) : options.inverse(this);
+    case '||':
+      return (v1 || v2) ? options.fn(this) : options.inverse(this);
+    default:
+      return options.inverse(this);
     }
   });
-}
-
-baadalApp.filterArrayObject = function(object, array_name, field_name, value) {
-  if (object.hasOwnProperty(array_name)) {
-    var array = object[array_name];
-    var newarray = [];
-    for ( var a in array) {
-      if (array[a][field_name] == value) {
-        newarray.push(array[a]);
-      }
-    }
-  }
-  object[array_name] = newarray;
-  return object;
-};
-
-baadalApp.populateSnapshots = function(data) {
-    var innerHTML = `{{#if snapshots}}
-                  {{#each snapshots}}
-                <tr data-snapshot-id="{{this.id}}">
-                  <td>{{this.name}}</td>
-                  <td>{{this.created}}</td>
-                  <td class="text-center">
-                    <a title="Restore this spanshot" href="#" class="snapshot-restore">
-                      <span class="badge">
-                        <span class="fa fa-reply"></span>
-                      </span>
-                    </a>
-                  </td>
-                </tr>
-                  {{/each}}
-              {{else}}
-                <tr><td colspan="100%" class="text-center"> There are no spanshots </td><tr>
-              {{/if}} `;
-    var template = Handlebars.compile(innerHTML);
-    var html = template(data);
-    return html;
-}
-
-baadalApp.getSettingsButtonsMarkup = function(status) {
-   var actions = {
-     'ACTIVE' : 'shutdown',
-     'SHUTOFF' : 'start',
-     'ERROR' : 'start'
-   };
-
-   var glyphicons = {
-     'ACTIVE' : 'stop',
-     'SHUTOFF' : 'play',
-     'ERROR' : 'play'
-   };
-   return '<div class="btn-group" role="group" aria-label="..."><button title="' +
-     actions[status] +
-     '" type="button" data-action="' +
-     actions[status] +
-     '" class="btn btn-success btn-action" aria-label="Start"><span class="fa fa-' +
-     glyphicons[status] +
-     '" aria-hidden="true"></span></button><button title="Settings" type="button"' +
-     'data-action="settings" class="btn btn-primary btn-settings" aria-label="Settings">' +
-     '<span class="fa fa-cog" aria-hidden="true"></span></button></div>';
 }
